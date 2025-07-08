782786cff2bed3f4f6d29689ee594feea24f9189
Jkaufman11 <kaufmanj@identificationmedia.com>
2025-07-08 18:26:16 -0400

    Initial Commit

---
+++ Artemis/ArtemisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtemisApp.swift
+//  Artemis
+//
+//  Created by Joshua Kaufman on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtemisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtemisTests/ArtemisTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtemisTests.swift
+//  ArtemisTests
+//
+//  Created by Joshua Kaufman on 7/8/25.
+//
+
+import Testing
+@testable import Artemis
+
+struct ArtemisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
